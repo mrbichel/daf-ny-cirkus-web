b4bd4b35719ae9546e78a5685e2de632b3010e10
Johan Bichel Lindegaard <public@johan.cc>
2021-06-30 18:13:01 +0200

    dynamic zoom level
    Signed-off-by: Johan Bichel Lindegaard <public@johan.cc>

--- src/lib/Mapd3/index.svelte
+++ src/lib/Mapd3/index.svelte
@@ -9,22 +9,33 @@
 	import { interpolate } from "d3-interpolate";
 	import {locations as location_data } from "../locations";
 	import { _ } from "lodash";
-  //import { geoVoronoi } from "d3-geo-voronoi"
-//  import {delaunay} from "d3-delaunay"
 
-	import dkgeo from './kommuner.geo.json' //'./dk.geo.json'; //'./rollercoaster.geo.json';
+// color palette 1: https://coolors.co/ef476f-06d6a0-ffd166-fcfcfc-26547c
+
+  //import { geoVoronoi } from "d3-geo-voronoi"
+  //import {delaunay} from "d3-delaunay"
+	import dkgeo from './map.geo.json' //'./dk.geo.json'; //'./rollercoaster.geo.json';
 
 	let data = [];
 	let width, height, initialWidth, initialHeight;
 	let g;
 	let svg, container;
 
-  const pinRadius = 8
+  const markerRadius = 8
 
   // TODO: reverse address lookup
-  const locations = location_data.map((d, i) => {
-    d.id = i
+  const locations = location_data.map( (d, i) => {
     d.active = false
+    d.id = i
+
+    const neighbours = location_data.filter(n => n != d).map( n => {
+      return {
+        distance: d3.geoDistance([n.lon, n.lat], [d.lon, d.lat]),
+        location: n
+      }
+    })
+    //d.closestNeighbours = _.sortBy( neighbours, 'distance');
+    d.closestNeighbour = _.sortBy( neighbours, 'distance')[0];
     return d
   })
 
@@ -40,15 +51,14 @@
     	g.attr("stroke-width", 1 / e.transform.k);
 
       markers
-      .attr("r", () => pinRadius / e.transform.k )
-      .style("stroke-width", () => (pinRadius*0.3) / e.transform.k )
-
+      .select(".circle")
+      //.transition().duration(80)
+      .attr("transform", () => `scale(${1/e.transform.k})`)
 
       popovers
 			//.style("left", (d) => ll2pT(transform, d)[0] + "px" )
 			.style("top",  (d) => ll2pT(e.transform, d)[1] + "px" )
-
-  	}
+  }
 
 	const land = dkgeo;
 	const projection = geoMercator()
@@ -73,14 +83,14 @@
 	function renderMap() {
 
 	  renderPath();
-
 	  popovers
 		//.style("left", (d) => ll2p(d)[0] + "px" )
 		.style("top",  (d) => ll2p(d)[1] + "px" )
 
 		markers
-			.attr( "cx", (d) => ll2p(d)[0]  )
-			.attr( "cy", (d) => ll2p(d)[1]  )
+      .attr("transform", (d) => "translate(" + ll2p(d)[0] + "," + ll2p(d)[1] + ")")
+			//.attr( "x", (d) => ll2p(d)[0]  )
+			//.attr( "y", (d) => ll2p(d)[1]  )
 	}
 
 	onMount(async function() {
@@ -93,6 +103,7 @@
     //svg.attr("width", width)
     //.attr("height", height)
     svg.attr("viewBox", [0, 0, width, height])
+    .on('click', outsideClick)
 
     svg.call(zoom)
 		g = svg.select(".zoom-container")
@@ -102,21 +113,28 @@
 		.data(land.features)
 		.enter()
 		.append("path")
-		.attr("fill", "#26547C")
-		//.attr("stroke", "none")
-		//.attr("stroke-width", 0.1)
 
     popovers = d3.selectAll(".popover")
 		.data(locations)
+    .classed("active", (d) => d.active)
+    .on("wheel", (e) => {
+      console.log("scroll", e)
+      e.preventDefault();
+      const eventClone = new e.constructor(e.type, {
+        clientX: e.clientX, 
+        clientY: e.clientY,
+        wheelDelta: e.wheelDelta,
+        wheelDeltaX: e.wheelDeltaX,
+        wheelDeltaY: e. wheelDeltaY})
+      svg.node().dispatchEvent(eventClone);
+    })
 
-		markers = g.select(".marker-layer").selectAll(".pin")
+    markers = g.select(".marker-layer").selectAll(".marker")
 			.data(locations)
-      .style("stroke-width", pinRadius*0.3)
-      .style("stroke-color", (d) => {
-        if(d.type == "school") {
-          return "#"
-        }
-      })
+      .classed("active", (d) => d.active)
+
+		markers
+      .select (".circle")
 			.on("click", clicked)
 			.on("mouseover", function(e, d){
         // TODO: rendering order, render last on top of other
@@ -131,6 +149,7 @@
     const cells = locations.map((d, i) => [[ll2p(d)[0], ll2p(d)[1]], voronoi.cellPolygon(i)]);
 
     //console.log(cells)
+    
     /*g.append("path")  
       .attr("fill", "none")
       .attr("stroke", "#ccc")
@@ -142,13 +161,32 @@
     .data(cells)
     .join("path")
       .attr("d", ([d, cell]) => `M${d3.polygonCentroid(cell)}L${d}`);
-    
-    g.append("path")
-      .attr("d", delaunay.renderPoints(null, 2));
-
     */
+    //g.append("path")
+    //  .attr("d", delaunay.renderPoints(null, 2));
+
+    
 
 	});
+
+
+
+  function outsideClick(e) {
+    const active = _.find(locations, (d) => d.active == true)
+    if(active) {
+      e.stopPropagation()
+      active.active = false;
+      
+      markers.classed("active", (d) => d.active)
+      popovers.classed("active", (d) => d.active)
+
+      d3.select(`#marker-${active.id}`).select(".circle").transition().duration(750)
+        .attrTween("d", (d) => {
+            const angleEnd= d3.interpolateNumber(90, 270);
+            return function(t) { return describeArc(0,0, markerRadius, -90, angleEnd(t)) };
+        })
+    }
+  }
 
 
 
@@ -156,41 +194,50 @@
       e.stopPropagation();
       //const [[x0, y0], [x1, y1]] = path.bounds(d);
       const point = projection([d.lon, d.lat]);
-      const minScale = 40
       const t0 = d3.zoomTransform(svg.node());
+      const outScale = (t0.k < 2) ? t0.k : t0.k *0.8;
+
+      // get pixel distance 
+      const pointNeighbour = projection([d.closestNeighbour.location.lon, d.closestNeighbour.location.lat]);
+      var a = pointNeighbour[0] - point[0];
+      var b = pointNeighbour[1] - point[1];
+      var distance = Math.sqrt(a * a + b * b);
+
+      const minScale =  markerRadius*3 / distance; // zoom in so dots are sperated by atleast 1 marker radius
       const scale = (t0.k > minScale) ? t0.k : minScale;
-
-      const outScale = (t0.k < 2) ? t0.k : t0.k *0.5;
-
-
-      popovers.transition().duration(750)
-      .style("opacity", 0)
-      .on("end", function(_d) {
-        d3.select('#popover-id-'+_d.id).style("display", "none")
-      });
 
       if(d.active) {
         d.active = false
-
         svg.transition().duration(750).call(
-        zoom.transform,
-        d3.zoomIdentity
-          .translate(-point[0]*outScale + width/2, -point[1]*outScale + height/2)
-          .scale(outScale),
-        d3.pointer(e, svg.node())
-      );
+          zoom.transform,
+          d3.zoomIdentity
+            .translate(-point[0]*outScale + width/2, -point[1]*outScale + height/2)
+            .scale(outScale),
+          d3.pointer(e, svg.node())        
+        );
+
+        d3.select(`#marker-${d.id}`).select(".circle").transition().duration(750)
+        .attrTween("d", (d) => {
+            const angleEnd= d3.interpolateNumber(90, 270);
+            return function(t) { return describeArc(0,0, markerRadius, -90, angleEnd(t)) };
+        })
 
 
       } else {
-        locations.forEach( (l) => l.active = false)
+        const active = _.find(locations, (d) => d.active == true)
+        if(active) {
+          active.active = false;
+          d3.select(`#marker-${active.id}`).select(".circle").transition().duration(750)
+            .attrTween("d", (d) => {
+                const angleEnd= d3.interpolateNumber(90, 270);
+                return function(t) { return describeArc(0,0, markerRadius, -90, angleEnd(t)) };
+            })
+        }
+        
         d.active = true
+        //console.log(d)
 
-        d3.select('#popover-id-'+d.id)
-        .style("display", "block") 
-        .transition().duration(750)
-        .style("opacity", 1)
-
-          svg.transition().duration(750).call(
+        svg.transition().duration(750).call(
           zoom.transform,
           d3.zoomIdentity
             .translate(-point[0]*scale + width/2, -point[1]*scale + height/2)
@@ -198,20 +245,38 @@
           d3.pointer(e, svg.node())
         );
 
+        d3.select(`#marker-${d.id}`).select(".circle").transition().duration(750)
+        .attrTween("d", (d) => {
+            const angleEnd= d3.interpolateNumber(270, 90);
+            return function(t) { return describeArc(0,0, markerRadius, -90, angleEnd(t)) };
+        })
+
       }
 
-      markers.classed("active", (m) => m.active)
-
-
-    
-    //svg.call(zoom.transform, t0);
-	 // .classed("active", true);
-	  //	.style("left", projection([d.lon, d.lat])[0] + "px" )
-	  //	.style("top", projection([d.lon, d.lat])[1] + "px" )
-
+      markers.classed("active", (d) => d.active)
+      popovers.classed("active", (d) => d.active)
 	  
     }
   
+  function polarToCartesian(x, y, r, angleDeg) {
+      const rad = (angleDeg-90) * Math.PI / 180.0;
+        return [
+          x + (r * Math.cos(rad)),
+          y + (r * Math.sin(rad))
+        ];
+  }
+
+
+  function describeArc(x, y, radius, startAngle=0, endAngle=360){
+
+      const start = polarToCartesian(x, y, radius, endAngle - 0.01);
+      const end = polarToCartesian(x, y, radius, startAngle);
+
+      const largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+      const arcSweep = endAngle - startAngle <= 180 ? '0' : '1';
+
+      return `M ${start[0]} ${start[1]} A ${radius} ${radius} 0 ${arcSweep} 0 ${end[0]} ${end[1]} ${(endAngle - startAngle === 360) ? 'Z' : ''}`;     
+  }
 
   var throttledResize = _.throttle(resizeMap, 800, {leading: false, trailing: true});
 
@@ -255,10 +320,8 @@
 
 // TODO: preserve center
     svg.call(zoom.transform, t0);
-
 		throttledResize.cancel();
 	}
-
 
   </script>
 
@@ -281,11 +344,16 @@
       </g>
 			<g class="marker-layer">
 			{#each locations as d}
-			<circle
-			  class="pin {d.type}"
-			  r={pinRadius}
-			   />
-		  	{/each}
+        <g class="marker {d.type}" id="marker-{d.id}">
+
+          <path class="circle" id="circle-{d.id}"
+              d="{describeArc(0,0,markerRadius)}"
+              stroke-width={markerRadius*0.6}
+          />
+
+
+        </g>
+		  {/each}
 			</g>
 
 		</g>
@@ -299,33 +367,59 @@
 		width: 100%;
 		height: 100%;
 		overflow: hidden;
-		background-color: "#FCFCFC";
+		background-color: #FCFCFC;
 	}
+
+  .map-layer {
+    fill: #26547C;
+  }
+
 	.popover {
 		position: absolute;
     left: 0;
     right: 0;
     margin-top: -1px;
-		opacity: 0;
-    display: none;
+		//opacity: 0;
+    //max-height: 0;
+    display: block;
+    overflow: hidden;
 		color: white;
     border-top:solid #FCFCFC 2px;
 		padding: 2px 30px;
-		background-color: rgba(0, 0, 0, 0.5);
-    pointer-events: none;
+    pointer-events: all;
 
+    transform: scaleY(0);    
+    transform-origin: top;
+    transition: transform 0.5s cubic-bezier(0.645, 0.045, 0.355, 1.000);
+
+    &.active {
+      transform: scaleY(1); 
+    }
     &.company {
       //border-color: #EF476F;
-      background-color: #EF476F;
+      background-color: rgba(#EF476F, 0.8);
     }
     &.school {
       //border-color: #06D6A0;
-      background-color: #06D6A0;
+      background-color: rgba(#06D6A0, 0.8);
     }
     &.festival {
       //border-color: #FFD166;
-      background-color: #FFD166;
+      background-color: rgba(#FFD166, 0.8);
     }
+
+    &.association {
+      background-color: rgba(#FFD166, 0.8);
+    }
+
+    &.support {
+
+    }
+
+    &.resstage {
+
+    }
+
     h1 {
       margin-top: 0.5em;
     }
@@ -333,38 +427,46 @@
       pointer-events: auto;
     }
 	}
-	.pin {
-		cursor: pointer;
-    stroke: #FCFCFC;
-    stroke-opacity: 0.2;
-		fill-opacity: 0.6;
-    &.company {
-      fill: #EF476F;
-    }
-    &.school {
-      fill: #06D6A0
-    }
-    &.festival {
-      fill: #FFD166
-    }
-
-    &:hover {
+	.marker {
+    .circle {
       cursor: pointer;
-      //fill: white;
-      fill-opacity: 1;
-      stroke-opacity: 0.8;
+      stroke-opacity: 0.2;
+      fill-opacity: 0.6;
+      transition-property: fill-opacity, stroke-opacity, fill, stroke;
+      transition-duration: 0.6s;
+      transition-timing-function: cubic-bezier(0.645, 0.045, 0.355, 1.000);
+
+      &:hover  {
+        cursor: pointer;
+        fill-opacity: 1;
+        stroke-opacity: 1;
+        transition-duration: 0.2s;
+      }
+
     }
-    &.active {
-      fill: #FCFCFC;
-      fill-opacity: 1;
+    &.company .circle {
+      fill: #EF476F;
+      stroke: lighten(#EF476F, 20);
+    }
+    &.school .circle {
+      fill: #06D6A0;
+      stroke: lighten(#06D6A0, 20);
+
+    }
+    &.festival .circle {
+      fill: #FFD166;
+      stroke: lighten(#FFD166, 20);
+    }
+    &.active .circle {
+      //fill: #26547C;
+      stroke: #FCFCFC;
+      fill-opacity: 0.9;
       stroke-opacity: 1;
     }
 
 	}
 
-  .polygons {
-        stroke: #444;
-    }
+
 
 
   </style>
