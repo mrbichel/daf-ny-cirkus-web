57f5f8d70341432877aa58f5384b58a0eae6b08e
sunniva23 <86781124+sunniva23@users.noreply.github.com>
2021-09-14 15:49:09 +0200

    Update locations.js

--- src/lib/locations.js
+++ src/lib/locations.js
@@ -6,9 +6,9 @@ export const locations = [
       address: "Enghavevej 82B, 2450 Copenhagen SV", 
       phone: "+45 32 54 20 95",
       email: "afuk@afuk.dk",
-      website: "https://afuk.dk", 
-      instagram: "https://instagram.com/artistlinien_afuk",
-      facebook: "https://facebook.com/afukremisen",
+      website: "www.afuk.dk", 
+      instagram: "www.instagram.com/artistlinien_afuk",
+      facebook: "www.facebook.com/afukremisen",
       lat: 55.66230820175756, lon: 12.539834935878755,  },
     { 
       type: "school",
@@ -17,9 +17,9 @@ export const locations = [
       address: "Olof Palmes Allé 39, 8200 Aarhus N",
       phone: "+45 87 40 86 00",
       email: "ab@fguaarhus.dk",
-      website: "https://fguaarhus.dk/musisk-og-kunstnerisk-produktion", 
-      instagram: "https://instagram.com/fgu.aarhus",
-      facebook: "https://facebook.com/FGUmidtby",
+      website: "www.fguaarhus.dk/musisk-og-kunstnerisk-produktion", 
+      instagram: "www.instagram.com/fgu.aarhus",
+      facebook: "www.facebook.com/FGUmidtby",
       lat: 56.18928838143087, lon: 10.182195269797058,  },
 
     { 
@@ -29,10 +29,11 @@ export const locations = [
         address: "Herrestedgade 6-8, 6520 Toftlund",
         phone: "+ 45 74 83 01 04",
         email: "kontor@musikogteater.dk",
-        website: "https://musikogteater.dk/scenekunst", 
-        instagram: "https://instagram.com/musikogteaterhoejskolen",
-        facebook: "https://facebook.com/musikogteater.dk",
-        lat: 55.19151330817705, lon: 9.065917498602422,  },
+        website: "www.musikogteater.dk/scenekunst", 
+        instagram: "www.instagram.com/musikogteaterhoejskolen",
+        facebook: "www.facebook.com/musikogteater.dk",
+        lat: 55.19151330817705, lon: 9.065917498602422,  
+    },
       {
         type: "company",
         name: "Glimt",
@@ -40,41 +41,55 @@ export const locations = [
         address: "c/o Foreningshuset Sundholm, Sundholmsvej 8, 2300 KBH S",
         phone: "+45 42 63 66 86",
         email: "info@glimt.info",
-        website: "https://glimt.info/da",
-        instagram: "https://instagram.com/glimt.info",
-        facebook: "https://facebook.com/glimt.info",
+        website: "www.glimt.info/da",
+        instagram: "www.instagram.com/glimt.info",
+        facebook: "www.facebook.com/glimt.info",
         lat: 55.661289604180695, lon: 12.5968790697805,  
       },
 
       {
         type: "company",
         name: "Borderline Circus",
-        about: "Borderline Circus is an artistic collective consisting of 3 funky artists with a lot of jazzy ideas and skills to impress. We may have high thoughts about ourselves, but we are ready to prove that we are right",
         address: "Borrelyngen 3, 2770 Kastrup",
         phone: "+45 61 66 87 94",
         email: "lalla@borderlinecircus.com",
-        facebook: "https://facebook.com/BorderlineCircus",
-        lat: 55.636173821145384, lon: 12.608307112106791,  
+        facebook: "www.facebook.com/BorderlineCircus",
+        lat: , lon: ,  
       },
-
+     {
+        type: "company",
+        name: "The Amazing Other",
+        about: "Playing, creating and performing, learning as much as we can, both circus, body moving, stage presence and stage combat, together we are the performing duo The Amazing Other.Lalla la Cour and Eivind Øverland is who we are and trapeze of all kinds is what we do: Duo, solo, static and swinging.",
+        address: "",
+        phone: "+45 61 66 87 94",
+        email: "lalla@borderlinecircus.com",
+        website: "www.theamazingother.simplesite.com",
+        instagram: "www.instagram.com/the.amazing.other",
+        facebook: "www.facebook.com/BorderlineCircus",
+        lat: , lon: 
+     },
+    
       {
         type: "company",
-        name: "Cikaros", 
-        website: "https://cikaros.dk/",
+        name: "Cikaros",
+        about: "“Cikaros is a collective with 3 circus artists that works to strengthen Danish new circus and put Danish circus art on the world map. Since its formation in 2005, Cikaros has produced and toured with new circus performances throughout Denmark and in Sweden, Norway, the Netherlands, France, Spain, Croatia, Russia, Egypt and Oman.”,
+        address: "Dagmarsgade 41, 4. tv, 2200 København N",
+        phone: "+45 29 82 69 60",  
         email: "cikaros@gmail.com",
-        address: "Dagmarsgade 41, 4. tv, 2200 København N", 
-        phone: "",  
+        website: "www.cikaros.dk",
+        facebook: "www.facebook.com/profile.php?id=100039202342357",
         lat: 55.69885739597575, lon: 12.545222540945232,  
       },
 
       {
         type: "company",
         name: "Teater Kimone", 
-        website: "http://kimone.dk/",
-        facebook: "https://https://facebook.com/TeaterKimone/",
+        about: "Welcome to Theater Kimone's Intergalactic Universe! We are a young theater company that with roots in new circuses makes playful circus theater for children and young people. Since we are human, what we do is often also about human beings, and all the beautiful, fun and difficult things about being human.",
+        address: "Jesper Brochmands Gade 4, 2200 København",
+        phone: "+45 21 29 24 60", 
         email: "info@kimone.dk",
-        address: "Jesper Brochmands Gade 4, 2200 København", 
-        phone: "",  
+        website: "www.kimone.dk",
+        facebook: "www.facebook.com/TeaterKimone",
         lat: 55.68861093552779, lon: 12.547068083271975,  
       },
 
@@ -82,51 +97,250 @@ export const locations = [
       {
         type: "company",
         name: "Rapid Eye", 
-        website: "http://rapideye.dk/",
-        facebook: "https://https://facebook.com/RapidEyeContemporaryCircus",
+        about: "Rapid Eye was founded in 2010 and has since managed to establish itself with a unique profile in modern performing arts. The company creates performances in the borderland between new circus, dance and visual theater. The performances are characterized by a love for man and his wonder about the world, and explore the perception of reality in a playful way.",
+        address: "Otto Busses Vej 5A, 2450 København SV",
+        phone: "+45 22 31 72 63",
         email: "info@rapideye.dk",
-        address: "Otto Busses Vej 5A, 2450 København SV", 
-        phone: "",  
+        website: "www.rapideye.dk",
+        facebook: "www.facebook.com/RapidEyeContemporaryCircus",
+        instagram: "www.instagram.com/rapideyetheatre",
         lat: 55.65969229060026, lon: 12.54942552745344,  
       },
 
       {
         type: "company",
-        name: "TinCanCompany", 
-        website: "tincancompany.dk",
-        instagram: "https://https://instagram.com/tincancompany/",
+        name: "TinCanCompany",
+        about: "TinCanCompany was founded by the Danish artists Rune Vadstrøm Andersen and Gry Lambertsen. For 14 years, they have built a razor-sharp stage timing and a distinctive aesthetic universe. With devilish violence, warmth of heart and humor, they climb Chinese poles, go on the slapline and manipulate everyday objects.",
+        adress: "Finlandkaj 6, 5000 Odense C", 
+        phone: " + 45 22 66 53 67",
         email: "info@tincancompany.dk",
-        address: "Finlandkaj 6, 5000 Odense C", 
-        phone: "",
+        website: "www.tincancompany.dk",
+        facebook: "www.facebook.com/tincancompany",
+        instagram: "www.instagram.com/tincancompany",
         lat: 55.40830551005386, lon: 10.379748540936205,  
       },
       {
         type: "company",
-        name: "X-act", 
-        website: "kittjohnson.dk",
-        instagram: "https://https://instagram.com/kittjohnsonxact/",
-        facebook: "https://https://facebook.com/kittjohnsonxact",
+        name: "Kitt Johnson X-act",
+        about: "Kitt Johnson X-act is one of the longest existing, most productive dance companies in Denmark. Founded in 1992, the company has evolved into a dynamic cultural enterprise, staging its own productions, producing and curating site-specific performance festivals, touring intensively at home and abroad, and offering physical classes, workshops, lectures and arts consultancy services.",
+        adress: "Struenseegade 15A, 3.th. 2200 København N",
+        phone: "+ 45 29 91 87 13",
         email: "x-act@kittjohnson.dk",
-        address: "Struenseegade 15A, 3.th. 2200 København N", 
-        phone: "",
+        website: "www.kittjohnson.dk",
+        facebook: "www.facebook.com/kittjohnsonxact",
+        instagram: "www.instagram.com/kittjohnsonxact",
         lat: 55.687098940204606, lon: 12.549599812108411,  
       },
       {
         type: "company",
-        name: "Sputnik & Wild Wonders", 
-        website: "http://sputnikandwildwonders.com/",
-        instagram: "https://https://instagram.com/sputnikandwildwonders/",
-        facebook: "https://https://facebook.com/sputnikandwildwonders/",
-        email: "",
-        address: "Egegade 18, 3 th. 2200 KBH N", 
-        phone: "",
+        name: "Sputnik & Wild Wonders",
+        about: "Our base is contemporary circus, with inspiration from the old style smokey salons, but with new redefined twists and concepts. To our wild wonders we invite fabulous and equally multi talented guest artists. We create and taylor sight specific cabarets, cross-over art collaborations, and are also available with our slack rope, acrobatic and cyr wheel skills to join inspiring projects.",
+        adress: "Egegade 18, 3 th, 2200 KBH N"
+        phone: "+ 45 26 80 76 88",
+        email: "sputnikandwildwonders@gmail.com",
+        website: "www.sputnikandwildwonders.com",
+        facebook: "www.facebook.com/sputnikandwildwonders",
+        instagram: "www.instagram.com/sputnikandwildwonders",
         lat: 55.690899512894866, lon: 12.558613898617903,  
       },
       {
         type: "company",
-        name: "Sparrow Dance", website: "http://sparrowdance.dk/", 
-        address: "Dansehallerne, H.C. Andersens Boulevard 25, 3. sal, DK-1553 København V", phone: "",
+        name: "Sparrow Dance"
+        about: "Sparrow Dance is an aerial dance and cross disciplinary stage art association formed by Esther Wrobel. The motivation behind Sparrow Dance is to promote and explore vertical dance and other suspended dance techniques to further expand the versatility of the performing arts. It aims to expand the possibilities of dance and develop the use of architecture as an active player in influencing movement.  Sparrow Dance aspires to challenge and play with the spatial perceptions of both the audience and the performers and explore alternate ways of looking at the world.",
+        adress: "(c/o) Dansehallerne, H.C. Andersens Boulevard 25, 3. sal, DK-1553 København V",
+        phone: "+ 45 31 75 03 09",
+        email: "sparrowdance.info@gmail.com",
+        website: "www.sparrowdance.dk",
+        facebook: "www.facebook.com/sparrowdance",
+        instagram: "www.instagram.com/sparrowdance.dk",
         lat: 55.674394029359206, lon: 12.571465254440337,  
       },
-      
+    {
+        type: "company",
+        name: "Out Of Balanz",
+        about: "Out of Balanz is an award-winning, international ensemble based in Copenhagen, recognized for their highly collaborative creative process and for producing original work and adaptations that are rich in spectacle and adventurous in form.",
+        adress: "Tjørnegade 9, 2200 København N",
+        phone: "+ 45 52 73 92 68",
+        email: "outofbalanz@gmail.com",
+        website: "www.outofbalanz.com",
+        facebook: "www.facebook.com/outofbalanz",
+        instagram: "www.instagram.com/outofbalanz",
+        lat: , lon:,
+      },
+    {
+        type: "company",
+        name: "Offspring Production",
+        about: "We want to nurture and increase the use of physical performing arts in Denmark and abroad. Coming from niches within moving, like circus and the dance style electric boogie, we are exploring ways of mixing styles and how we can incorporate more storytelling in nonverbal expression forms.",
+        adress: "Va11a, Siljangade 3, 2300 København S,
+        phone: "+ 45 20 30 21 95",
+        email: "mail@offspringproduction.dk",
+        website: "www.offspringproduction.dk",
+        instagram: "www.instagram.com/offspringproduction",
+        lat: , lon:,
+      },
+    {
+        type: "company",
+        name: "Vice Versa Circus",
+        about: "Danish circus company by two static trapeze artists.",
+        adress: "Trunderup Dongsvej 30, Kværndrup",
+        phone: 
+        email: "triotriskela@gmail.com",
+        website: 
+        facebook: "www.facebook.com/Viceversacircus",
+        instagram: "www.instagram.com/viceversacircus",
+        lat: , lon:,
+      },
+    {
+        type: "company",
+        name: "Volantes",
+        about: "Volantes is a new-thinking concept in performing arts with a special focus on circus skills. Our concept is to reach out to people with live performances and shows that will make the individual go home with an extraordinary feeling of inspiration and passion.",
+        adress: "Skjulhøj Allé 21, 2720 København",
+        phone: " +45 30 35 29 54",
+        email: "djvolantes@gmail.com",
+        website: "www.volantes.dk",
+        instagram: "www.instagram.com/volantescompany",
+        lat: , lon:,
+      },
+    {
+        type: "company",
+        name: "Tanter",
+        about: "Tanter consists of Karoline (a strong fast rope artist), Moa (a slackrope artist with taste of drama) and Elise (a freak-magnet specialised in swinging trapeze and contortion). Three wild women who want to change the world for the better.",
+        adress: "Va11a, Siljangade 3, 2300 København S",
+        phone: "+ 45 22 88 67 96",
+        email: "tanter@tanter.dk",
+        website: "www.tanter.dk",
+        facebook: "www.facebook.com/tanterkompani",
+        instagram: "www.instagram.com/tanterkompani",
+        lat: , lon:,
+      },
+          {
+        type: "company",
+        name: "Right Way Down"
+        about: "Experimenting with the absurdity of reality upturned, you are invited to see the world the right way down.",
+        adress: "Finlandkaj 6, 5000 Odense C",
+        phone: "+45 22 96 50 79",
+        email: "rightwaydowncircus@gmail.com",
+        website: "www.rightwaydown.com",
+        facebook: "www.facebook.com/rightwaydown",
+        instagram: "www.instagram.com/rightwaydowncircus",
+        lat: , lon:,
+      },
+        {
+        type: "association"
+        name: "Cirkus Panik"
+        about: "Cirkus Panik is one of the few surviving old-fashioned clown families and circus blood flows in our veins. We have been touring around and abroad for more than 35 years, with both classic circus performances, circus schools and workshops as well as rental of unique cotton circus tents.",
+        adress: "Skjulhøj Allé 21, 2720 Vanløse",
+        phone: "+ 45 24 27 93 40",
+        email: "cirkus@cirkuspanik.dk",
+        website: "www.cirkuspanik.dk",
+        facebook: "www.facebook.com/cirkuspanik",
+        instagram: "www.instagram.com/cirkuspanik",
+        lat: , lon:,
+      },
+        {
+        type: "association",
+        name: "Cirkus Tværs",
+        about: "Cirkus Tværs is a fully equipped children's and youth circus based in Gellerup, Brabrand. We work with the circus as a pedagogical tool, and thereby help children and young people to develop competencies, both personal, social, practical and "professional.",
+        adress: "Gudrunsvej 78, 8220 Brabrand",
+        phone: "+ 45 86 25 87 80",
+        email: "cirkus@mail.dk",
+        website: "www.cirkustvaers.dk",
+        instagram: "www.instagram.com/cirkustvaers",
+        lat: , lon:,
+      },
+        {
+        type: "association",
+        name: "Ørkenfortet",
+        about: "Our training is based on the joy of movement in a non-competitive space, where community, trust and attention are a big part of the activities. Here we work with the individual child at their level within floor and air acrobatics.",
+        adress: "Enghavevej 82B, 2450 København",
+        phone: "+ 45 24 26 24 50",
+        email: "fortet@afuk.dk",
+        website: "www.afuk.dk/foreninger/hold/",
+        instagram: "www.instagram.com/afuk.orkenfortet/",
+        lat: , lon:,
+      },
+          {
+        type:
+        name: 
+        about:
+        adress: 
+        phone:
+        email:
+        website:
+        facebook:
+        instagram:
+        lat: , lon:,
+      },
+          {
+        type:
+        name: 
+        about:
+        adress: 
+        phone:
+        email:
+        website:
+        facebook:
+        instagram:
+        lat: , lon:,
+      },
+          {
+        type:
+        name: 
+        about:
+        adress: 
+        phone:
+        email:
+        website:
+        facebook:
+        instagram:
+        lat: , lon:,
+      },
+          {
+        type:
+        name: 
+        about:
+        adress: 
+        phone:
+        email:
+        website:
+        facebook:
+        instagram:
+        lat: , lon:,
+      },
+          {
+        type:
+        name: 
+        about:
+        adress: 
+        phone:
+        email:
+        website:
+        facebook:
+        instagram:
+        lat: , lon:,
+      },
+          {
+        type:
+        name: 
+        about:
+        adress: 
+        phone:
+        email:
+        website:
+        facebook:
+        instagram:
+        lat: , lon:,
+      },
+          {
+        type:
+        name: 
+        about:
+        adress: 
+        phone:
+        email:
+        website:
+        facebook:
+        instagram:
+        lat: , lon:,
+      },
   ];
