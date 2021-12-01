cf33858494dc0006fa93cec59b5148eea6aef5f3
Johan Bichel Lindegaard <public@johan.cc>
2021-12-01 18:00:03 +0100

    pull logo right

--- src/lib/Navigation/index.svelte
+++ src/lib/Navigation/index.svelte
@@ -11,7 +11,7 @@
 			<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
 			<li class:active={$page.path === '/articles'}><a sveltekit:prefetch href="/articles">Articles</a></li>
 			<li class:active={$page.path === '/contact'}><a sveltekit:prefetch href="/contact">Contact</a></li>
-			<li>DAF logo</li>
+			<li id="daf-logo"><a rel="external" href="https://artisten.dk">DAF</a></li>
 
 		</ul>
 	</nav>
@@ -30,10 +30,14 @@
 			ul {
 				margin: 0.1em 0;
 				padding: 0;
-				display: inline-flex;
+				display: flex;
 				li {
 					padding: 0.2em 0.5em;
 					list-style: none;
+				}
+
+				#daf-logo {
+					margin-left: auto;
 				}
 			}
 		}
