f12f772f90d2678ae68eadaeb45d5c3d6872f1f1
Johan Bichel Lindegaard <public@johan.cc>
2021-12-02 10:48:34 +0100

    mongodb

--- src/lib/mongodb-client.ts
+++ src/lib/mongodb-client.ts
@@ -3,56 +3,39 @@ dotenv.config();
 
 import mongoose from 'mongoose'
 
-//import { MongoClient } from 'mongodb';
 
 const uri = process.env['MONGODB_URI'];
+if (!uri) {
+    throw new Error('Please add your Mongo URI to .env')
+}
 
 const options = {
     logger: console.log,
-    loggerLevel: 'debug',
+    loggerLevel: process.env['NODE_ENV'] === 'development' ? 'debug' : 'warn',
     useNewUrlParser: true, 
-    useUnifiedTopology: true, 
+    useUnifiedTopology: true,
   }
-
-mongoose.connection.on('error', e => {
-    console.error(e)
-})
-
-mongoose.connection.once('open', () => {
-    console.log(`mongoose successfully connected to ${uri}`)
-})
 
 connect().catch(err => console.log(err));
 async function connect() {
-    if (!uri) {
-        throw new Error('Please add your Mongo URI to .env')
+    if(mongoose.connection.readyState === 1) {
+        return 
     }
 
-    mongoose.set('debug', true)
+    if(process.env['NODE_ENV'] !== 'production') {
+        mongoose.set('debug', process.env['NODE_ENV'] === 'development' )
+    }
+    
+    mongoose.connection.on('error', e => {
+        console.error(e)
+    })
+    
+    mongoose.connection.once('open', () => {
+        console.log(`mongoose successfully connected to ${uri}`)
+    })
 
     await mongoose.connect(uri, options)
-
 }
 
 
-
-/*if (process.env['NODE_ENV'] === 'development') {
-    // In development mode, use a global variable 
-    // so that the value is preserved across module reloads 
-    // caused by HMR (Hot Module Replacement).
-    if (!global._mongoClientPromise) {
-        client = mongoose.connect(uri, options);
-        global._mongoClientPromise = client.connect()
-    }
-    connectionPromise = global._mongoClientPromise
-} else {
-    // In production mode, it's best to 
-    // not use a global variable.
-    client = new MongoClient(uri, options)
-    connectionPromise = client.connect()
-}*/
-
-// Export a module-scoped MongoClient promise. 
-// By doing this in a separate module, 
-// the client can be shared across functions.
 export default connect
