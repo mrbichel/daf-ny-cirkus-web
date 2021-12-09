821cefadda8db7526cf838f386f66ba184522d65
Johan Bichel Lindegaard <public@johan.cc>
2021-12-09 16:08:45 +0100

    cache tweak

--- src/hooks.ts
+++ src/hooks.ts
@@ -5,23 +5,31 @@ export async function handle({ request, resolve }) {
 	//request.locals.user = await getUserInformation(request.headers.cookie);
     await connect()
 
+    const extraHeaders = {
+        'Cache-control': 'max-age=0, s-maxage=86400'
+    }
+
     const path = request.path.replace(/\//, "")
+    
     if(path === '' || path.match(/^[\w-]+$/) ) {
         request.path = `/${path}`
     } 
+    
+    else if( path.includes("api/") ) {
+        extraHeaders['Cache-Control'] = 'max-age=0, s-maxage=5 stale-while-revalidate=120'
+    }
 
     /*if(request.path === '/service-worker.js') {
         return
     }*/
 
 	const response = await resolve(request);
-    
 
 	return {
 		...response,
 		headers: {
 			...response.headers,
-            'Cache-Control': 'max-age=0, s-maxage=86400'
+            ...extraHeaders
 		}
 	};
 }
